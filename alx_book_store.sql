41f2e537423e9ba6fcdeec89d52f0543f246873d
Achuba <achubadaniel45@gmail.com>
2025-12-14 00:45:13 +0100

    Changed the foreign key of customer

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -27,7 +27,7 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+    FOREIGN KEY (customer_id) REFERENCES customer (customer_id)
 );
 
 CREATE TABLE Order_Details (
